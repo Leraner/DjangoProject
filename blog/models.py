41f77de74f7f4dc16bb5808a9b68922f513a312d
Leraner <legend.tony@yandex.ru>
2019-12-28 00:40:40 +0300

    binding models

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from mptt.models import MPTTModel
+from mptt.fields import TreeForeignKey
 
 
 class Tag(models.Model):
     name = models.CharField('Имя', max_length=100)
     slug = models.SlugField('url', max_length=100, unique=True)
+    published = models.BooleanField('Отображать?', default=True)
 
     def __str__(self):
         return self.name
@@ -13,8 +18,92 @@ class Tag(models.Model):
         verbose_name_plural = 'Теги'
 
 
+class Category(MPTTModel):
+    """Модель категории"""
+    name = models.CharField('Имя', max_length=100)
+    slug = models.SlugField('url', max_length=100)
+    # default='' - позволяет не удалять миграции
+    description = models.TextField('Опимание', max_length=1000, default='', blank=True)
+    # Подкатегории, => наследуется от MPTTModel
+    parent = TreeForeignKey(
+        'self',
+        verbose_name='Родительская категория',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='children'
+    )
+    # default - уже адресс
+    template = models.CharField('Шаблон', max_length=500, default='blog/post_list.html')
+    published = models.BooleanField('Отображать?', default=True)
+    paginated = models.PositiveIntegerField('Кол-во новостей на странице', default=5)
+    sort = models.PositiveIntegerField('Порядок', default=0)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Post(models.Model):
+    """Модель поста"""
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    title = models.CharField('Заголовок', max_length=250)
+    mini_text = models.TextField('Краткое содержание', max_length=5000)
+    text = models.TextField('Текст', max_length=10000000)
+    create_data = models.DateTimeField('Дата создания', auto_now_add=True)
+    subtitle = models.TextField('Под заголовок', max_length=500, blank=True, null=True)
+    slug = models.SlugField('url', max_length=100, unique=True)
+    tags = models.ManyToManyField(Tag, verbose_name='Тэг', blank=True)
+    category = models.ForeignKey(
+        Category,
+        verbose_name='Категория',
+        on_delete=models.CASCADE,
+        null=True,
+    )
+    image = models.ImageField('Главная фотография', upload_to='post/', null=True, blank=True)
+    edit_date = models.DateTimeField(
+        'Дата редактирования',
+        default=timezone.now,
+        blank=True,
+        null=True
+    )
+    published_date = models.DateTimeField(
+        'Дата публикации',
+        default=timezone.now,
+        blank=True,
+        null=True
+    )
+    template = models.CharField('Шаблон', max_length=500, default='news/post_detail.html')
+    published = models.BooleanField('Отображать?', default=True)
+    viewed = models.PositiveIntegerField('Просмотрено', default=0)
+    status = models.BooleanField('Для зарегестрированных', default=False)
+    sort = models.PositiveIntegerField('Порядок', default=0)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+
+
 class Comment(models.Model):
     """Модель комментария"""
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор',
+        on_delete=models.CASCADE,
+    )
+    post = models.ForeignKey(Post, verbose_name='Статья', on_delete=models.CASCADE)
     text = models.TextField('Текст')
     create_data = models.DateTimeField('Дата создания', auto_now_add=True)
     slug = models.SlugField('url', max_length=100, unique=True)
@@ -26,32 +115,3 @@ class Comment(models.Model):
     class Meta:
         verbose_name = 'Комментарий'
         verbose_name_plural = 'Комментарии'
-
-
-class Post(models.Model):
-    """Модель поста"""
-    title = models.CharField('Заголовок', max_length=250)
-    mini_text = models.TextField('Описание')
-    text = models.TextField('Текст')
-    create_data = models.DateTimeField('Дата создания', auto_now_add=True)
-    slug = models.SlugField('url', max_length=100, unique=True)
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        verbose_name = 'Пост'
-        verbose_name_plural = 'Посты'
-
-
-class Category(models.Model):
-    """Модель категории"""
-    name = models.CharField('Имя', max_length=100)
-    slug = models.SlugField('url', max_length=100)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,6 @@ from .views import HomeView, CategoryView, PostView
 
 
 urlpatterns = [
-    path('<slug:post_slug>/', PostView.as_view(), name='post'),
-    path('<slug:category_slug>/', CategoryView.as_view(), name='category'),
     path('', HomeView.as_view(), name='home'),
+    path('<slug:category_slug>/', CategoryView.as_view(), name='category'),
 ]
