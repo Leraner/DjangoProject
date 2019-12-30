10a694b50f16779abfb1320f746c353ae7f499f5
Leraner <legend.tony@yandex.ru>
2019-12-30 18:49:31 +0300

    connected TagView and CategoryView

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from .views import *
 
 
 urlpatterns = [
-    path('tag/<slug:tag_slug>/', TagView.as_view(), name='tag'),
+    path('tag/<slug:tag_slug>/', CategoryView.as_view(), name='tag'),
     path('<slug:category>/<slug:post_slug>/', PostDetailView.as_view(), name='detail_post'),
     path('<slug:category_slug>/', CategoryView.as_view(), name='category'),
     path('', HomeView.as_view(), name='home'),
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.http import HttpResponse
 from django.views.generic.base import View
 from django.utils import timezone
+
 
 
 from .models import Category, Post, Comment, Tag
@@ -11,33 +12,38 @@ from .models import Category, Post, Comment, Tag
 class HomeView(View):
     """Вывод полной домашней страницы"""
     def get(self, request):
-        category_list = Category.objects.all()
+        category_list = Category.objects.filter(published=True)
         # __lte - меньше или равно
         post_list = Post.objects.filter(published_date__lte=timezone.now(), published=True)
         return render(request, 'blog/home.html', {'categories': category_list, 'posts': post_list})
 
 
 class CategoryView(View):
-    """Вывод статей гатегории"""
-    def get(self, request, category_slug):
-        current_category = Category.objects.get(slug=category_slug)
-        posts = Post.objects.filter(
+    """Вывод статей гатегории и тегов"""
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now(), published=True)
+
+    def get(self, request, category_slug=None, tag_slug=None):
+        posts = []
+        if category_slug is not None:
             # __ - хотим обратиться в модель на которую завязаны
-            category__slug=category_slug, category__published=True, published=True
-        )
-        return render(request, posts.first().get_category_template(), {'posts': posts})
+            posts = self.get_queryset().filter(category__slug=category_slug, category__published=True)
+        elif tag_slug is not None:
+            posts = self.get_queryset().filter(tags__slug=tag_slug, tags__published=True)
+        else:
+            posts = self.get_queryset()
+        # exists - спрашивает есть ли хоть одна статья в категории
+        if posts.exists():
+            template = posts.first().get_category_template()
+        else:
+            template = 'blog/post_list.html'
+
+        return render(request, template, {'posts': posts})
 
 
 class PostDetailView(View):
     """Вывод поста"""
-    def get(self, request, category, post_slug):
-        categories = Category.objects.all()
-        post = Post.objects.get(slug=post_slug, published_date__lte=timezone.now(), published=True)
+    def get(self, request, **kwargs):
+        categories = Category.objects.filter(published=True)
+        post = get_object_or_404(Post, slug=kwargs.get('post_slug'), published_date__lte=timezone.now(), published=True)
         return render(request, post.template, {'post': post, 'categories': categories})
-
-
-class TagView(View):
-    """Вывод постов по тегам"""
-    def get(self, request, tag_slug):
-        posts = Post.objects.filter(tags__slug=tag_slug, published=True)
-        return render(request, posts.first().get_category_template(), {'posts': posts})
